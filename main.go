102a69b19cf9efd18430f77368c28cd58f3ca7df
Fahim Williams <fahimwilliams@Fahims-MacBook-Pro.attlocal.net>
2025-05-23 10:40:48 -0400

    Added scoreAvg function.

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+type student struct {
+	name  string
+	score int
+}
+
+func scoreAvg(scores []float64) (result float64) {
+	for _, score := range scores {
+		result += score
+	}
+	if len(scores) == 0 {
+		return 0
+	}
+	return result / float64(len(scores))
+}
+
+func main() {
+
+}
